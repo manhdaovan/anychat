ab01ff7fc38104e0fdcbb746e1ff2060e87a4566
Manh DV <manhdv@upty.jp>
2017-03-21 18:23:16 +0900

    Add validate to user model and controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,28 @@
 class HomeController < ApplicationController
   def index
+    @user = User.new
   end
 
   def login
-
+    @user = User.new(login_params)
+    if @user.valid?
+      user = User.find_by(username: @user.username)
+      if user.nil?
+        @user.save
+        flash[:success] = 'Your account has been created!'
+        redirect_to root_path
+      else
+        if user.authenticate(login_params.fetch(:password, nil))
+          flash[:success] = 'Welcome to your anychat!'
+          redirect_to root_path
+        else
+          @user.errors.add(:password, 'invalid')
+          render :index
+        end
+      end
+    else
+      render :index
+    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string(100)      not null
+#  password_digest :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+  USERNAME_REGEX = /[a-zA-Z0-9]/
+  has_secure_password
+
+  validates :username, presence: true, length: {minimum: 6, maximum: 50}, format: USERNAME_REGEX
+  validates :password, presence: true, length: {minimum: 8, maximum: 50}
 end
