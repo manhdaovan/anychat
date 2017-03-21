a0b6b76b31079205bff3a09784bf76616e1e11d4
Manh DV <manhdv@upty.jp>
2017-03-21 19:01:58 +0900

    Add channels

---
+++ app/assets/javascripts/channels/one2_one.js
@@ -0,0 +1,11 @@
+App.one2_one = App.cable.subscriptions.create("One2OneChannel", {
+    connected: function() {
+        //Called when the subscription is ready for use on the server
+    },
+    disconnected: function() {
+        //Called when the subscription has been terminated by the server
+    },
+    received: function(data) {
+        //Called when there's incoming data on the websocket for this channel
+    }
+});
