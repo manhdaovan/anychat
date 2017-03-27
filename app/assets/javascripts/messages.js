cf41fe3f07b4eb8adb9d75d17761bcb27f27fe4b
Manh DV <manhdv@upty.jp>
2017-03-27 12:02:51 +0900

    Add some logic on views

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,18 +1,33 @@
 $(document).on('click', '.list-user-item', function () {
-    console.log("dmmmOUT");
-    buildMessageBox('manhdv', 'manhdv222');
+    var $this = $(this);
+    var msgBox = $('#any-messages-boxes');
+    msgBox.loading(true);
+    $.ajax({
+        url: '/users/online',
+        type: 'post',
+        data: {username: $this.data('username')},
+        success: function (data) {
+            buildMessageBox('manhdv', 'manhdv222', data);
+        },
+        error: function () {
+            toastr.error('Error has been occurred. Please reload page and try again latter!');
+        },
+        complete: function(){
+            msgBox.loading(false);
+        }
+    });
 });
 
-function buildMessageBox(from_username, to_username) {
-    console.log("dmmm");
+function buildMessageBox(from_username, to_username, extra_data) {
+    console.log("extra_data", extra_data);
     $('.message-box-item').hide();
     var box = $('#msg-box-for-' + from_username + '-and-' + to_username);
     if (box.length > 0) {
-        box.show();
+        box.show(500);
     } else {
         var boxHtml = $($('#message-box-template').html());
         boxHtml.attr('id', 'msg-box-for-' + from_username + '-and-' + to_username);
-        boxHtml.show();
+        boxHtml.show(500);
         $('#any-messages-boxes').append(boxHtml);
     }
 }
--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,1 +1,8 @@
 toastr.options.closeButton = true;
+$.fn.loading = function(show) {
+    if(show){
+        console.log('show loading');
+    }else{
+        console.log('hide loading');
+    }
+};
